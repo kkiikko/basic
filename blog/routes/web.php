518b29468fb85e6a7d5076497bdee643ba8e5c2c
kkiikko <kainosuke1234@gmail.com>
2021-10-07 20:32:36 +0900

    08-2

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -10,8 +10,4 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/posts', 'PostController@index');
+Route::get('/','PostController@index');
